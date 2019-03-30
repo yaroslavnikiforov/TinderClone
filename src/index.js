021b239c1e506edc0cfe1e8c7d665f77675156e7
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-03-30 16:26:52 +0200

    using data from firebase

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ const styles = StyleSheet.create({
 class App extends Component {
   state = {
     profileIndex: 0,
+    profiles: [],
   }
 
   componentWillMount() {
@@ -28,11 +29,21 @@ class App extends Component {
       .database()
       .ref()
       .child('users')
-      .once('value', snap => console.info('Data: ', snap.val()))
+      .once('value', snap => {
+        let profiles = []
+
+        snap.forEach(profile => {
+          const { id, name, bio, birthday } = profile.val()
+
+          profiles.push({ id, name, bio, birthday })
+        })
+
+        this.setState({ profiles })
+      })
   }
 
   render() {
-    const { profileIndex } = this.state
+    const { profileIndex, profiles } = this.state
 
     return (
       <View style={styles.container}>
@@ -55,48 +66,3 @@ class App extends Component {
 }
 
 export default App
-
-const profiles = [
-  {
-    id: '259389830744794',
-    name: 'Candice',
-    birthday: '10/18/1986',
-    bio: 'Supermodel',
-  },
-  {
-    id: '720115413',
-    name: 'Alessandra',
-    birthday: '1/10/1989',
-    bio: 'Dancer',
-  },
-  {
-    id: '169571172540',
-    name: 'Miranda',
-    birthday: '12/12/1983',
-    bio: 'Doctor',
-  },
-  {
-    id: '1476279359358140',
-    name: 'Alissa',
-    birthday: '2/11/1990',
-    bio: 'Comedian',
-  },
-  {
-    id: '10155639858650271',
-    name: 'Behati',
-    birthday: '3/23/1991',
-    bio: 'Developer',
-  },
-  {
-    id: '912478262117011',
-    name: 'Rosie',
-    birthday: '9/4/1989',
-    bio: 'Artist',
-  },
-  {
-    id: '173567062703796',
-    name: 'Kendall',
-    birthday: '8/17/1992',
-    bio: 'Truck Driver',
-  },
-]
