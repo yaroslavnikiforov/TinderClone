3d6677416975c41afdf1c2c5c5ba53858964c7c5
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-04-20 16:03:08 +0300

    login with facebook

--- app/screens/Login/index.js
+++ app/screens/Login/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Facebook } from "expo";
 import { View } from "react-native";
 
 import FacebookButton from "../../components/FacebookButton";
@@ -10,12 +11,29 @@ class Login extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <FacebookButton onPress={this._onFacebookButtonPress} />
+        <FacebookButton onPress={this._login} />
       </View>
     );
   }
 
   _onFacebookButtonPress = () => this.props.navigation.navigate("Home");
+
+  _login = async () => {
+    const APP_ID = "2182360415187709";
+    const options = { permissions: ["public_profile"] };
+    const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+      APP_ID,
+      options
+    );
+
+    if (type === "success") {
+      const response = await fetch(
+        `https://graph.facebook.com/me?access_token=${token}`
+      );
+
+      console.log(await response.json());
+    }
+  };
 }
 
 export default Login;
