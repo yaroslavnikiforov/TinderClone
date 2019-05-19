1a97b89eb502263a5943baac6dd131054891d4d3
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-05-19 14:15:35 +0300

    fetching messages from firebase

--- app/screens/Chat/index.js
+++ app/screens/Chat/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { View } from "react-native";
 import firebase from "firebase";
+import { forEach } from "lodash";
 import { GiftedChat } from "react-native-gifted-chat";
 
 class Chat extends Component {
@@ -17,11 +18,12 @@ class Chat extends Component {
 
   render() {
     const { messages, user } = this.state;
+    const avatar = `https://graph.facebook.com/${user.id}/picture?height=80`;
 
     return (
       <GiftedChat
         messages={messages}
-        user={{ _id: user.uid }}
+        user={{ _id: user.uid, avatar }}
         onSend={this._onSendMessage}
       />
     );
@@ -34,6 +36,8 @@ class Chat extends Component {
       user.uid > profile.uid
         ? `${user.uid}-${profile.uid}`
         : `${profile.uid}-${user.uid}`;
+
+    this._watchChat();
   }
 
   _onSendMessage = message =>
@@ -42,6 +46,20 @@ class Chat extends Component {
       .ref("messages")
       .child(this.chatId)
       .push({ ...message[0], createdAt: Date.now() });
+
+  _watchChat = () =>
+    firebase
+      .database()
+      .ref("messages")
+      .child(this.chatId)
+      .on("value", snap => {
+        let messages = [];
+
+        forEach(snap.val(), message => messages.push(message));
+        messages.reverse();
+
+        this.setState({ messages });
+      });
 }
 
 export default Chat;
